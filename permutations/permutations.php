4cfb72a83d149447e31be693f755871b671f7b15
Rankod <akronder@gmail.com>
2018-02-02 20:33:40 +0100

    permutations opened

---
+++ permutations/permutations.php
@@ -0,0 +1,5 @@
+<?php 
+function permutations(string $s): array 
+{
+  // Your code here
+}
---
+++ permutations/tests/permutationsTest.php
@@ -0,0 +1,19 @@
+<?php
+include "./permutations.php";
+
+use PHPUnit\Framework\TestCase;
+
+final class PermutationsTest extends TestCase {
+    private function assertEquivalent(array $expected, array $actual, string $msg = '') 
+    {
+        sort($expected);
+        sort($actual);
+        $this->assertEquals($expected, $actual, $msg);
+    }
+    public function testDescriptionExamples() 
+    {
+        $this->assertEquivalent(['a'], permutations('a'));
+        $this->assertEquivalent(['ab', 'ba'], permutations('ab'));
+        $this->assertEquivalent(['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'], permutations('aabb'));
+    }
+}
