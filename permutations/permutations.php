79208f16bc8bef570c0184bc7c69d4d378eab356
Rankod <akronder@gmail.com>
2018-02-03 18:12:04 +0100

    wstepne dane

--- permutations/permutations.php
+++ permutations/permutations.php
@@ -1,5 +1,35 @@
 <?php 
 function permutations(string $s): array 
 {
-  // Your code here
+         
 }
+
+$s = '123';
+$lenght = strlen($s);
+
+function permutation($lenght)
+{
+    $permutations = 1;
+    
+    for($i = 1; $i <= $lenght; $i++){
+         $permutations *= $i;
+    }
+    return $permutations;
+}
+
+$permutations = permutation($lenght);
+$charsAppearance = $permutations / strlen($s);
+
+for($i = 0; $i < $lenght; $i++){
+   $arrStr[] =  $s[$i]; 
+}
+
+echo "Ilość permutacji: $permutations";
+echo "<br>";
+echo "Ilość wystąpień jednego znaku: $charsAppearance";
+echo "<br>";
+echo "Lista znaków: ";
+foreach($arrStr as $word){
+    echo "$word, \n";
+}
+ 
