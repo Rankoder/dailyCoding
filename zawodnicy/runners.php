160bb1e60d60c0819b8d2cbb49bb6a25453c89d5
Rankod <akronder@gmail.com>
2018-03-07 13:58:31 +0100

    dodano tablice z zawodnikami i utworzono wstepne klasy

---
+++ zawodnicy/runners.php
@@ -0,0 +1,44 @@
+<?php
+$runners = [
+    [
+        'id' => 1,
+        'maxSpeed' => 20,
+        'condition' => 15,
+        'speedDecrise' => 2      
+    ],
+    
+    [
+        'id' => 2,
+        'maxSpeed' => 22,
+        'condition' => 17,
+        'speedDecrise' => 3      
+    ],
+    
+    [
+        'id' => 3,
+        'maxSpeed' => 18,
+        'condition' => 12,
+        'speedDecrise' => 2      
+    ],
+    
+    [
+        'id' => 4,
+        'maxSpeed' => 13,
+        'condition' => 17,
+        'speedDecrise' => 4      
+    ],
+    
+    [
+        'id' => 5,
+        'maxSpeed' => 21,
+        'condition' => 13,
+        'speedDecrise' => 1      
+    ], 
+    
+    [
+        'id' => 6,
+        'maxSpeed' => 20,
+        'condition' => 9,
+        'speedDecrise' => 2      
+    ]
+];
