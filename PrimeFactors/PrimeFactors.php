dc4a2a221cd32d03d0dff6e3711d6fdfffc33eab
Rankod <akronder@gmail.com>
2019-01-14 21:40:55 +0100

    refactoring

--- PrimeFactors/PrimeFactors.php
+++ PrimeFactors/PrimeFactors.php
@@ -5,7 +5,7 @@ function primeFactors($number) {
    $start = 2;
     while($start <= $number) {
         $counter = 0;
-        primeCheck($start);    
+          
         while ($number % $start === 0) {       
             $number /= $start; 
             $counter++;
@@ -16,15 +16,7 @@ function primeFactors($number) {
     return $string;
 }
 
-function primeCheck($divider){ 
-    if ($divider != 1) {    
-        for ($i = 2; $i <= $divider/2; $i++){ 
-            if ($divider % $i == 0) 
-                break; 
-        } 
-    return $divider; 
-    }
-} 
+
 
 function convertToString($counter, $prime) {
     if($counter === 1) {
@@ -33,4 +25,7 @@ function convertToString($counter, $prime) {
         return "(" . $prime . "**" . $counter . ")";
     }
 }
+
+$number = 86240;
+echo primeFactors($number);
 ?>
