10ead04af6bbf5aa290f76eb44cb98dadd75562c
Rankod <akronder@gmail.com>
2019-02-18 12:21:19 +0100

    Ceasar Cipher refactoring

--- CaesarCipher/CaesarCipher.php
+++ CaesarCipher/CaesarCipher.php
@@ -3,23 +3,19 @@ class CaesarCipher {
     public $shift;
     private $alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
-    public function __construct($lenght )
-    {
+    public function __construct($lenght ) {
         $this->shift = $lenght;
     }
     
-    function encode($str)
-    {  
+    function encode($str) {  
         return strtr(strtoupper($str), $this->alphabet, $this->createAlphabet());
     }
     
-    function decode($str)
-    {        
+    function decode($str) {        
        return strtr(strtoupper($str), $this->createAlphabet(), $this->alphabet);
     }
     
-    function createAlphabet()
-    {
+    function createAlphabet() {
         return substr($this->alphabet, $this->shift, strlen($this->alphabet) - $this->shift).substr($this->alphabet, 0, $this->shift);
     }    
 }
