44951ebfe13e25cef0dec0a5cd3167683e27ba37
Rankod <akronder@gmail.com>
2019-06-06 16:23:41 +0200

    added function changeColor

--- LangtonsAnt/LangtonAnt.php
+++ LangtonsAnt/LangtonAnt.php
@@ -1,6 +1,22 @@
 <?php
+
 function ant($grid, $column, $row, $n, $dir = 0): array {
-        $grid = [[0, 0]];
-        return $grid;
+    $actualAntPosition = [
+        'column'    => $column,
+        'row'       => $row
+    ];
+    $actualColor = $grid[0][0];
+
+    $grid[$column] = [$row, $actualColor];
+    $grid[$column] = [$row, changeColor($actualColor)];
+
+    return $grid;
 }
 
+function changeColor($actualColor) {
+    if($actualColor === 1) {
+        return 0;
+    } elseif($actualColor === 0) {
+        return 1;
+    }
+}
